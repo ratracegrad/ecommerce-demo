3726d4fc947fbbcae469d07f9f80dd5f9330e606
Jennifer Bland <ratracegrad@gmail.com>
2017-05-20 12:31:42 -0400

    Added server file that will start application

---
+++ server.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/* require modules */
+const app = require('./server/app');
+const http = require('http');
+const port = process.env.PORT || 3000; // TODO port this in .env
+
+/* start server and set event listening function */
+const server = http.createServer(app);
+server.listen(port);
+
+server.on('listening', onListening);
+server.on('error', onError);
+
+/* server event methods below */
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+
+};
+
+function onListening() {
+  console.log(`Server listening on port ${port}`);
+};
